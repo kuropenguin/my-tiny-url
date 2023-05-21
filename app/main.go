9ee100b4ab241b269099609bacaea970d7504c4c
kuropenguin <kuro1994.t@gmail.com>
2023-05-21 21:20:50 +0900

    add generate random string

--- app/main.go
+++ app/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	baseStr    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	baseStrLen = len(baseStr)
+)
+
 func main() {
-	println("Hello World")
+	rand.Seed(time.Now().UnixNano())
+	// TODO POST URL
+	// TODO GET URL
+	GenerateRandomString()
+}
+
+func GenerateRandomString(str string) string {
+	randomNumber := rand.Intn(baseStrLen)
+	return string(str[randomNumber])
 }
