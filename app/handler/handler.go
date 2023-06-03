6c5226b5e6c5de0de3b11252eeb0ae3c38a5dd01
kuropenguin <kuro1994.t@gmail.com>
2023-06-03 21:33:19 +0900

    CreateTinyURL 対応

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -1,18 +1,66 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/kuropenguin/my-tiny-url/app/entity"
+	"github.com/kuropenguin/my-tiny-url/app/usecase"
 )
 
-// TODO request validation
-
-func CreateTinyURL(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello World")
+type IHanlder interface {
+	CreateTinyURL(w http.ResponseWriter, r *http.Request)
+	GetOriginURLByTinyURL(w http.ResponseWriter, r *http.Request)
 }
 
-func GetOriginURLByTinyURL(w http.ResponseWriter, r *http.Request) {
+type HandlerImpl struct {
+	usecase usecase.IUseCase
+}
+
+func NewHandlerImple(usecase usecase.IUseCase) *HandlerImpl {
+	return &HandlerImpl{usecase: usecase}
+}
+
+type createTinyResponse struct {
+	TinyURL string `json:"tiny_url"`
+}
+type createTinyRequest struct {
+	OriginURL string `json:"origin_url"`
+}
+
+// TODO request validation
+// curl -X POST -H "Content-Type: application/json" -d '{"origin_url":"google.com"}' localhost:8080/create_tiny_url
+func (h *HandlerImpl) CreateTinyURL(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	var createTinyRequest createTinyRequest
+	if err := json.Unmarshal(reqBody, &createTinyRequest); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	tinyURL, err := h.usecase.CreateTinyURL(entity.OriginURL(createTinyRequest.OriginURL))
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	// write response 200 and json
+	res := createTinyResponse{TinyURL: string(tinyURL)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+	w.WriteHeader(http.StatusOK)
+
+}
+
+// TODO request validation
+func (h *HandlerImpl) GetOriginURLByTinyURL(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello World")
 }
--- app/main.go
+++ app/main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"log"
+
 	"github.com/gorilla/mux"
 	"github.com/kuropenguin/my-tiny-url/app/handler"
+	"github.com/kuropenguin/my-tiny-url/app/repository"
+	"github.com/kuropenguin/my-tiny-url/app/usecase"
 )
 
 func main() {
 	router := mux.NewRouter()
+	repository := repository.NewMapRepository()
+	usecase := usecase.NewUsecaseImpl(repository)
+	handler := handler.NewHandlerImple(usecase)
 	router.HandleFunc("/create_tiny_url", handler.CreateTinyURL).Methods("POST")
 	router.HandleFunc("/get_origin_url", handler.GetOriginURLByTinyURL).Methods("GET")
 
+	log.Println("start server")
 	http.ListenAndServe(fmt.Sprintf(":%d", 8080), router)
 	// TODO graceful shutdown
 }
