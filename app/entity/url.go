d53e627076d050ff98e1da366a305facf1f4dfde
kuropenguin <kuro1994.t@gmail.com>
2023-06-03 16:34:30 +0900

    add entity

---
+++ app/entity/url.go
@@ -0,0 +1,31 @@
+package entity
+
+import "math/rand"
+
+const (
+	baseURL    = "http://localhost:8080/"
+	baseStr    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	baseStrLen = len(baseStr)
+	tinyLen    = 8
+)
+
+type TinyURL string
+
+type OriginURL string
+
+func GenerateTinyURL() TinyURL {
+	return TinyURL(baseURL + generateRandomString(tinyLen))
+}
+
+func generateRandomString(loopNum int) string {
+	url := ""
+	for i := 0; i < loopNum; i++ {
+		url += generateRandomChar(baseStr)
+	}
+	return url
+}
+
+func generateRandomChar(str string) string {
+	randomNumber := rand.Intn(baseStrLen)
+	return string(str[randomNumber])
+}
