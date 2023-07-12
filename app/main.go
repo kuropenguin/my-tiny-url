8aaf27460e406cceaf73718b16d6ebd9e6d70c2d
kuropenguin <kuro1994.t@gmail.com>
2023-07-12 20:39:56 +0900

    delete 不要な初期化

--- app/main.go
+++ app/main.go
@@ -31,8 +31,6 @@ func main() {
 
 	log.Println("start server")
 
-	redis.NewRedisClient()
-
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
