b5918fcae9df8978d516650e0d8fa607263e64b2
kuropenguin <kuro1994.t@gmail.com>
2023-12-16 16:02:31 +0900

    add crypt.tls

--- app/redis/redis.go
+++ app/redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"context"
+	"crypto/tls"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -12,10 +13,13 @@ var (
 )
 
 func NewRedisClient() *redis.Client {
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     "tiny-url-aiwe7o.serverless.apne1.cache.amazonaws.com:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "my-tiny-url-aiwe7o.serverless.apne1.cache.amazonaws.com:6379", // Redisサーバーのアドレス
+		Password: "",                                                             // パスワードがあれば設定（ElastiCacheでは通常不要）
+		DB:       0,                                                              // 使用するDB
+		TLSConfig: &tls.Config{ // トランジット中の暗号化を使用するためのTLS設定
+			MinVersion: tls.VersionTLS12, // TLS1.2以上を使用する設定
+		},
 	})
 
 	return rdb
