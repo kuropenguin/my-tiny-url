557b3e2faf31e7347b3a3931df38dbd62eb1918e
kuropenguin <kuro1994.t@gmail.com>
2023-12-21 20:16:16 +0900

    change log

--- app/main.go
+++ app/main.go
@@ -34,7 +34,7 @@ func main() {
 	router.HandleFunc("/create_tiny_url", handler.CreateTinyURL).Methods("POST")
 	router.HandleFunc("/get_original_url", handler.GetOriginalURLByTinyURL).Methods("GET")
 
-	log.Println("start server")
+	log.Println("start server ...")
 
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
