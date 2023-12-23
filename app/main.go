300f57607ae6c12eda59b56d6aa6ff349c475e8b
kuropenguin <kuro1994.t@gmail.com>
2023-12-23 16:56:20 +0900

    change log

--- app/main.go
+++ app/main.go
@@ -25,6 +25,8 @@ func main() {
 	// request validation
 	// logging
 
+	log.Println("start server ...")
+
 	router := mux.NewRouter()
 	repo := repository.NewMysqlRepository(mysql.GetDB())
 	cache := repository.NewCacheRedisRepository(redis.NewRedisClient())
@@ -33,8 +35,6 @@ func main() {
 
 	router.HandleFunc("/create_tiny_url", handler.CreateTinyURL).Methods("POST")
 	router.HandleFunc("/get_original_url", handler.GetOriginalURLByTinyURL).Methods("GET")
-
-	log.Println("start server ...")
 
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
