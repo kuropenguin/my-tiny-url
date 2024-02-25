dae320efed65a6daaeec53147561d897b4d26920
kuropenguin <kuro1994.t@gmail.com>
2024-02-25 21:09:27 +0900

    delete drop table

--- mysql/initdb.d/01_schema.sql
+++ mysql/initdb.d/01_schema.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS `urls`;
-
 CREATE TABLE `urls` (
     `original_url` varchar(512) NOT NULL, `tiny_url` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
