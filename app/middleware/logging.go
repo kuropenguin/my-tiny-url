a61eaa2937432be648a634390d09da2a93afee24
kuropenguin <kuro1994.t@gmail.com>
2024-03-03 21:35:04 +0900

    add Logging middleware

--- app/middleware/logging.go
+++ app/middleware/logging.go
@@ -5,11 +5,9 @@ import (
 	"net/http"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler {
+func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// リクエストに関する情報をログに記録
 		log.Printf("Request received: %s %s", r.Method, r.URL.Path)
-		// 次の処理（次のミドルウェアまたは最終的なハンドラ）へリクエストを渡す
 		next.ServeHTTP(w, r)
 	})
 }
