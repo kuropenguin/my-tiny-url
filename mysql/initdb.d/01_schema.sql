666fd16754d2a5af2c5edd75a791b1cdf6004617
kuropenguin <kuro1994.t@gmail.com>
2024-03-03 13:05:16 +0900

    test table change

--- mysql/initdb.d/01_schema.sql
+++ mysql/initdb.d/01_schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE `urls` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE `test_table` (
-    `hoge_url` varchar(512) NOT NULL, `diff` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
+    `test` varchar(512) NOT NULL, `diff` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
