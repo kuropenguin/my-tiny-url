3a0a4c99d28a5da81ded134aab77dcd11a257263
kuropenguin <kuro1994.t@gmail.com>
2024-03-03 12:38:42 +0900

    schema 変更あれば起動するように

--- mysql/initdb.d/01_schema.sql
+++ mysql/initdb.d/01_schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE `urls` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE `test_table` (
-    `original_url` varchar(512) NOT NULL, `tiny_url` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
+    `hoge_url` varchar(512) NOT NULL, `tiny_url` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
