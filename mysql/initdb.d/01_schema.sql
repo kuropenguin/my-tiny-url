a8121f96af1de820fde0a2661c0ea6f165e3c711
kuropenguin <kuro1994.t@gmail.com>
2024-03-03 17:45:36 +0900

    change primary

--- mysql/initdb.d/01_schema.sql
+++ mysql/initdb.d/01_schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE `urls` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE `hoge_table` (
-    `test` varchar(512) NOT NULL, `diff` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`original_url`)
+    `test` varchar(512) NOT NULL, `diff` varchar(255) NOT NULL, `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`test`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
