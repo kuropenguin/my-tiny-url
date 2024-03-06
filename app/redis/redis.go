defe2eb98707138d9605c068094d433ca2f7d64a
kuropenguin <kuro1994.t@gmail.com>
2024-03-06 19:46:43 +0900

    success redi connect

--- app/redis/redis.go
+++ app/redis/redis.go
@@ -2,8 +2,8 @@ package redis
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
+	"log"
 
 	"github.com/kuropenguin/my-tiny-url/app/config"
 	"github.com/redis/go-redis/v9"
@@ -16,17 +16,18 @@ var (
 
 func NewRedisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		// Addr:     "my-tiny-url-aiwe7o.serverless.apne1.cache.amazonaws.com:6379", // Redisサーバーのアドレス
 		Addr:     fmt.Sprintf("%s:%d", config.Reids.Host, config.Reids.Port),
 		Password: config.Reids.Password,
 		DB:       config.Reids.DB, // 使用するDB
-		TLSConfig: &tls.Config{ // トランジット中の暗号化を使用するためのTLS設定
-			MinVersion: tls.VersionTLS12, // TLS1.2以上を使用する設定
-		},
+		// TLSConfig: &tls.Config{ // トランジット中の暗号化を使用するためのTLS設定
+		// MinVersion: tls.VersionTLS12, // TLS1.2以上を使用する設定
+		// },
 	})
+
 	if err := rdb.Ping(ctx).Err(); err != nil {
-		panic(rdb.Ping(ctx).Err())
+		panic(err)
 	}
+	log.Println("Connected to Redis!")
 
 	return rdb
 }
